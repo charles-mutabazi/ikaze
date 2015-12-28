6c1a4498751f044fbf6921b8aa10e973c2ebc644
Mutabazi C <charl.mutabazi@gmail.com>
2015-12-28 23:48:40 +0900

    fixing omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,7 @@ if Rails.env.development?
   OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 end
 
-# OmniAuth.config.full_host = Rails.env.production? ? 'https://domain.com' : 'http://localhost:3000'
+OmniAuth.config.full_host = Rails.env.production? ? 'https://domain.com' : 'http://localhost.example.com'
 
 # Rails.application.config.middleware.use OmniAuth::Builder do
 #   # provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"]
